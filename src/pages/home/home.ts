8708417051fccd718b1389c59df85c3bea0eb7e2
payelkhaskel <payelkhaskel99@gmail.com>
2018-12-19 10:30:10 +0530

    1st commit

---
+++ src/pages/home/home.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { ForgotpasswordPage } from '../forgotpassword/forgotpassword';
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
